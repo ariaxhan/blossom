c243082cf6825e98dec18179d622877456a8b8d0
Aria Han <ariaxhan@gmail.com>
2024-06-16 22:54:23 -0700

    Initial Commit

---
+++ blossom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blossom
+//
+//  Created by Aria Han on 6/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blossom/blossomApp.swift
@@ -0,0 +1,17 @@
+//
+//  blossomApp.swift
+//  blossom
+//
+//  Created by Aria Han on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct blossomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
